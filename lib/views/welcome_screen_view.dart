dee5559c2940a70a4f0d5bc49fffcc3754ec1af1
ladin hastürel <lhasturel@gmail.com>
2022-05-21 18:51:51 +0300

    20.05.2022

--- lib/views/welcome_screen_view.dart
+++ lib/views/welcome_screen_view.dart
@@ -1,56 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:hrms/storage/text_storage.dart';
-import 'package:hrms/themes/padding.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:hrms/views/main_screen_view.dart';
 
-class WelcomeView extends StatefulWidget {
+class WelcomeView extends StatelessWidget {
   const WelcomeView({Key? key}) : super(key: key);
 
   @override
-  State<WelcomeView> createState() => _WelcomeViewState();
-}
-
-class _WelcomeViewState extends State<WelcomeView> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: null,
-        body: Padding(
-          padding: ProjectPadding.pagePaddingAll,
-          child: Column(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.02,
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Image.asset(
-                'assets/images/welcomeFirst.png',
-                fit: BoxFit.fitHeight,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Text(
-                WelcomeTexts.welcomeText,
-                style: Theme.of(context).textTheme.headline3,
-              ),
-              const SizedBox(
-                height: 80,
-              ),
-              Container(
-                alignment: Alignment.bottomRight,
-                padding: const EdgeInsets.only(right: 20),
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/login');
-                  },
-                  icon: const Icon(Icons.arrow_forward_outlined),
-                  label: Text(WelcomeTexts.buttonText),
-                ),
-              ),
-            ],
-          ),
-        ));
+    return AnimatedSplashScreen(
+      splash: 'assets/images/welcomeFirst.png',
+      splashIconSize: 120,
+      backgroundColor: Colors.white38,
+      nextScreen: const MainView(),
+      splashTransition: SplashTransition.slideTransition,
+      pageTransitionType: PageTransitionType.fade,
+    );
   }
 }
